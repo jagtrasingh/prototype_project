c4f4d36fd2704507aca4e699a711631fd7c6fcdd
singh Jagtar <jagtarcgc.mca11@gmail.com>
2019-02-06 09:43:11 +0000

    Add master detail app

---
+++ webapp/test/integration/AllJourneys.js
@@ -0,0 +1,20 @@
+// We cannot provide stable mock data out of the template.
+// If you introduce mock data, by adding .json files in your webapp/localService/mockdata folder you have to provide the following minimum data:
+// * At least 3 Products in the list
+// * All 3 Products have at least one Category
+
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"./arrangements/Arrangement",
+	"./MasterJourney",
+	"./NavigationJourney",
+	"./NotFoundJourney",
+	"./BusyJourney"
+], function (Opa5, Arrangement) {
+	"use strict";
+	Opa5.extendConfig({
+		arrangements: new Arrangement(),
+		viewNamespace: "open.sap.MasterDetail.view.",
+		autoWait: true
+	});
+});
